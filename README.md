5560c19c71f18242145784d50696e0ed90ca03eb
lingulist <mattis.list@lingulist.de>
2021-11-24 06:54:23 +0100

    update new files

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
-# hruschkaturkic
-Dataset by Hruschka et al. (2015) on Turkic languages
+# Dataset hruschkaturkic
+
+## How to cite
+
+If you use these data please cite
+this dataset using the DOI of the [particular released version](../../releases/) you were using
+
+## Description
+
+
+## Statistics
+
+
+![Glottolog: 100%](https://img.shields.io/badge/Glottolog-100%25-brightgreen.svg "Glottolog: 100%")
+![Concepticon: 0%](https://img.shields.io/badge/Concepticon-0%25-red.svg "Concepticon: 0%")
+![Source: 0%](https://img.shields.io/badge/Source-0%25-red.svg "Source: 0%")
+![BIPA: 100%](https://img.shields.io/badge/BIPA-100%25-brightgreen.svg "BIPA: 100%")
+![CLTS SoundClass: 100%](https://img.shields.io/badge/CLTS%20SoundClass-100%25-brightgreen.svg "CLTS SoundClass: 100%")
+
+- **Varieties:** 26
+- **Concepts:** 222
+- **Lexemes:** 4,213
+- **Sources:** 0
+- **Synonymy:** 1.00
+- **Cognacy:** 4,213 cognates in 222 cognate sets (3 singletons)
+- **Cognate Diversity:** 0.00
+- **Invalid lexemes:** 0
+- **Tokens:** 15,667
+- **Segments:** 60 (0 BIPA errors, 0 CTLS sound class errors, 60 CLTS modified)
+- **Inventory size (avg):** 33.27
+
+## Possible Improvements:
+
+
+
+- Entries missing sources: 4213/4213 (100.00%)
+
+## CLDF Datasets
+
+The following CLDF datasets are available in [cldf](cldf):
+
+- CLDF [Wordlist](https://github.com/cldf/cldf/tree/master/modules/Wordlist) at [cldf/cldf-metadata.json](cldf/cldf-metadata.json)
--- lexibank_hruschkaturkic.py
+++ lexibank_hruschkaturkic.py
@@ -1,32 +1,73 @@
-# coding=utf-8
-from __future__ import unicode_literals, print_function
-from itertools import groupby
-
-import attr
-import lingpy
-from pycldf.sources import Source
-from clldutils.path import Path
+from pathlib import Path
 from clldutils.misc import slug
 from pylexibank.dataset import Dataset as BaseDataset
+from pylexibank import Concept, Language
+from pylexibank.forms import FormSpec
+from pylexibank import progressbar
+import attr
 
-from pylexibank.dataset import Metadata, Concept
-from pylexibank.util import pb
+#@attr.s
+#class CustomConcept(Concept):
+#    Chinese_Gloss = attr.ib(default=None)
+
+
+#@attr.s
+#class CustomLanguage(Language):
+#    Location = attr.ib(default=None)
 
 class Dataset(BaseDataset):
     dir = Path(__file__).parent
+    id = "hruschkaturkic"
 
-    def cmd_install(self, **kw):
-        with self.cldf as ds:
-            data = {}
-            for taxon, string in pb(
-                    self.raw.read_tsv('TurkicFullBoundaries.txt')):
-                out = []
-                for char in string:
-                    if char == '-':
-                        out += ['-']
-                    else:
-                        out += [char]
-                out = ' '.join(out)
-                data[taxon] = [''.join(x).strip() for x in out.split(':')]
+    def cmd_makecldf(self, args):
+
+        mapper = {
+                row["Grapheme"]: [row["IPA"], row["IPAInSource"]] for row in self.etc_dir.read_csv(
+                "orthography.tsv", delimiter="\t",
+                dicts=True)}
+        data = {}
+        for taxon, string in progressbar(
+                self.raw_dir.read_csv('TurkicFullBoundaries.txt', delimiter="\t"),
+                desc='reading data'
+                ):
+            out = []
+            for char in string:
+                if char == '-':
+                    out += ['-']
+                else:
+                    out += [char]
+            out = ' '.join(out)
+            data[taxon] = [''.join(x).strip().split() for x in out.split(':')]
+
+        for i in range(len(data[taxon])):
+            args.writer.add_concept(
+                    ID="word-{0}".format(i+1),
+                    Name="Etymon {0}".format(i+1)
+                    )
+
+        for language in progressbar(self.languages):
+            args.writer.add_language(
+                    ID=language["ID"],
+                    Name=language["Name"],
+                    Glottocode=language["Glottocode"] or ""
+                    )
+            for i, word in enumerate(data[language["ID"]]):
+                alignment = [mapper.get(t, [t])[0] for t in word]
+                tokens = [t for t in alignment if t != "-"]
+                value = [mapper.get(t, [t, t])[1] for t in word]
+                if tokens:
+                    lex = args.writer.add_form_with_segments(
+                        Language_ID=language["ID"],
+                        Parameter_ID="word-{0}".format(i+1),
+                        Value="".join(value),
+                        Form=" ".join(word),
+                        Segments=tokens,
+                        Cognacy=i+1
+                        )
+                    args.writer.add_cognate(
+                        lexeme=lex,
+                        Cognateset_ID=i+1,
+                        Alignment=alignment,
+                        )
 
 
